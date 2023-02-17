32386c854188c89628d7ff9595e4a46290c28993
Makinde Olayinka <olayinkam30@gmail.com>
2023-02-17 00:40:40 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - A prgram that prints a line with puts function
- *
+ * main - A prgram that prints a line with puts function=
  * Return: 0 (Success)
  */
 int main(void)
-(
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-)
+}
