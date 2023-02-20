80fbeb5c4ddc424555be4efbe158540c771aff44
Makinde Olayinka <olayinkam30@gmail.com>
2023-02-20 02:36:26 -0800

    go 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int n, m;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
@@ -18,7 +19,7 @@ int main(void)
 	}
 	else if (m < 6 && m != 0)
 	{
-		printf("Last gigit of %d is %d and is less than 6 and not 0\n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
 	else
 	{
