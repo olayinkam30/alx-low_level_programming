2b43a72545a695015f023abefb547e383fc2c49b
Makinde Olayinka <olayinkam30@gmail.com>
2023-02-20 10:45:35 -0800

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,11 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
+{
         int ch;
-        
+
         for (ch = 'a', ch <= 'z', ch++)
-        {       
+        {
                 if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
