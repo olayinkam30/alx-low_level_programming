5bda223e0806188b8fb8f136d1df6ce8bfb3b6ff
Makinde Olayinka <olayinkam30@gmail.com>
2023-02-21 10:29:11 -0800

    go 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints alphabet in lowercase
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z';)
+		_putchar('i');
+	_putchar('\n');
+}
